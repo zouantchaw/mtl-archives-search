4be5ef5894d7e2055726ba2642c9d61931ca4c63
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2026-01-31 16:16:59 -0500

    Lower map z-index to show overlays

--- apps/next-app/components/ui/map.tsx
+++ apps/next-app/components/ui/map.tsx
@@ -204,7 +204,7 @@ function Map({
             attributionControl={false}
             zoomControl={false}
             className={cn(
-                "z-50 size-full min-h-96 flex-1 rounded-md",
+                "size-full min-h-96 flex-1 rounded-md",
                 className
             )}
             {...props}
